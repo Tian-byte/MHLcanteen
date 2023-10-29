98b46be57d38a3a5fe9115fcc748bef809dee4d7
点点 <102277652+1474416640@users.noreply.github.com>
2023-10-29 23:47:00 +0800

    1.0

---
+++ src/com/tianedu/mhl/dao/MenuDao.java
@@ -0,0 +1,15 @@
+package com.tianedu.mhl.dao;
+
+import com.tianedu.mhl.domain.Menu;
+
+/**
+ * @author tian
+ */
+public class MenuDao extends BasicDAO<Menu>{
+
+
+
+
+
+
+}
---
+++ src/com/tianedu/mhl/domain/Menu.java
@@ -0,0 +1,49 @@
+package com.tianedu.mhl.domain;
+
+/**
+ * @author tian
+ *
+ * 该类(javaben) 和 menu 表对应
+ *
+ */
+public class Menu {
+    private Integer id;
+    private String name;
+    private String type;
+    private Double price;
+
+    public Menu() {  // 无参构造器
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
